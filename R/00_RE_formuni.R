5cd97bbf53757e9ad5d70565832e629de5120fe8
Carles Milà <carles.milagarcia@bsc.es>
2025-06-23 09:55:26 +0200

    RE models and FE exploratory

--- R/00_RE_formuni.R
+++ R/00_RE_formuni.R
@@ -15,13 +15,13 @@ all_baseline <- ""
 # 1. Spatial random effects ----
 
 re_s01 <- paste("f(hr_id, model = 'bym2', graph = g, scale.model = TRUE,",
-                "hyper = precision.prior)")
+                "hyper = precision.prior, constr = TRUE)")
 re_s02 <- paste("f(hr_id, model = 'bym2', graph = g, scale.model = TRUE,",
-                "hyper = precision.prior2)")
-# re_s03 <- paste("f(hr_id, model = 'bym2', replicate = 'nino_id', graph = g,",
-#                 "scale.model = TRUE, hyper = precision.prior)")
-# re_s04 <- paste("f(hr_id, model = 'bym2', replicate = 'nino_id', graph = g,",
-#                 "scale.model = TRUE, hyper = precision.prior2)")
+                "hyper = precision.prior2, constr = TRUE)")
+re_s03 <- paste("f(hr_id, model = 'bym2', replicate = 'nino_id', graph = g,",
+                "scale.model = TRUE, hyper = precision.prior, constr = TRUE)")
+re_s04 <- paste("f(hr_id, model = 'bym2', replicate = 'nino_id', graph = g,",
+                "scale.model = TRUE, hyper = precision.prior2, constr = TRUE)")
 
 # 2. Seasonal random effects ----
 re_w01 <- paste("f(week_id, model = 'rw1', cyclic = TRUE, constr = TRUE,",
@@ -60,73 +60,72 @@ re_w14 <- paste("f(week_id, model = 'rw2', cyclic = TRUE, constr = TRUE,",
                 "scale.model = TRUE, hyper = precision.prior2)")
 
 # 3. Yearly random effects ----
-re_y01 <- "f(year_id, model = 'iid', hyper = precision.prior)"
-re_y02 <- "f(year_id, model = 'rw1', hyper = precision.prior)"
-re_y03 <- "f(year_id, model = 'rw2', hyper = precision.prior)"
+re_y01 <- "f(year_id, model = 'iid', hyper = precision.prior, constr = TRUE)"
+re_y02 <- "f(year_id, model = 'rw1', hyper = precision.prior, constr = TRUE)"
+re_y03 <- "f(year_id, model = 'rw2', hyper = precision.prior, constr = TRUE)"
 
 re_y04 <- paste("f(year_id, model = 'iid', replicate = state_id, ",
-                "hyper = precision.prior)")
+                "hyper = precision.prior, constr = TRUE)")
 re_y05 <- paste("f(year_id, model = 'rw1', replicate = state_id, ",
-                "hyper = precision.prior)")
+                "hyper = precision.prior, constr = TRUE)")
 re_y06 <- paste("f(year_id, model = 'rw2', replicate = state_id, ",
-                "hyper = precision.prior)")
+                "hyper = precision.prior, constr = TRUE)")
 
 re_y07 <- paste("f(year_id, model = 'iid', replicate = region_id, ",
-                "hyper = precision.prior)")
+                "hyper = precision.prior, constr = TRUE)")
 re_y08 <- paste("f(year_id, model = 'rw1', replicate = region_id, ",
-                "hyper = precision.prior)")
+                "hyper = precision.prior, constr = TRUE)")
 re_y09 <- paste("f(year_id, model = 'rw2', replicate = region_id, ",
-                "hyper = precision.prior)")
+                "hyper = precision.prior, constr = TRUE)")
 
 re_y10 <- paste("f(year_id, model = 'iid', replicate = biome_id, ",
-                "hyper = precision.prior)")
+                "hyper = precision.prior, constr = TRUE)")
 re_y11 <- paste("f(year_id, model = 'rw1', replicate = biome_id, ",
-                "hyper = precision.prior)")
+                "hyper = precision.prior, constr = TRUE)")
 re_y12 <- paste("f(year_id, model = 'rw2', replicate = biome_id, ",
-                "hyper = precision.prior)")
+                "hyper = precision.prior, constr = TRUE)")
 
 re_y13 <- paste("f(year_id, model = 'iid', replicate = koppen_id, ",
-                "hyper = precision.prior)")
+                "hyper = precision.prior, constr = TRUE)")
 re_y14 <- paste("f(year_id, model = 'rw1', replicate = koppen_id, ",
-                "hyper = precision.prior)")
+                "hyper = precision.prior, constr = TRUE)")
 re_y15 <- paste("f(year_id, model = 'rw2', replicate = koppen_id, ",
-                "hyper = precision.prior)")
+                "hyper = precision.prior, constr = TRUE)")
 
-re_y16 <- "f(year_id, model = 'iid', hyper = precision.prior2)"
-re_y17 <- "f(year_id, model = 'rw1', hyper = precision.prior2)"
-re_y18 <- "f(year_id, model = 'rw2', hyper = precision.prior2)"
-
+re_y16 <- "f(year_fct, model = 'iid', hyper = precision.prior2, constr = TRUE)"
+re_y17 <- "f(year_id, model = 'rw1', hyper = precision.prior2, constr = TRUE)"
+re_y18 <- "f(year_id, model = 'rw2', hyper = precision.prior2, constr = TRUE)"
 
 # 4. Temporal random effects ----
-re_t01 <- "f(time_id, model = 'rw1', hyper = precision.prior)"
-re_t02 <- "f(time_id, model = 'rw2', hyper = precision.prior)"
-re_t03 <- "f(time_id, model = 'ar1', hyper = precision.prior)"
+re_t01 <- "f(time_id, model = 'rw1', hyper = precision.prior, constr = TRUE)"
+re_t02 <- "f(time_id, model = 'rw2', hyper = precision.prior, constr = TRUE)"
+re_t03 <- "f(time_id, model = 'ar1', hyper = precision.prior, constr = TRUE)"
 
-re_t04 <- "f(time_id, model = 'rw1', replicate = state_id, hyper = precision.prior)"
-re_t05 <- "f(time_id, model = 'rw2', replicate = state_id, hyper = precision.prior)"
-re_t06 <- "f(time_id, model = 'ar1', replicate = state_id, hyper = precision.prior)"
+re_t04 <- "f(time_id, model = 'rw1', replicate = state_id, hyper = precision.prior, constr = TRUE)"
+re_t05 <- "f(time_id, model = 'rw2', replicate = state_id, hyper = precision.prior, constr = TRUE)"
+re_t06 <- "f(time_id, model = 'ar1', replicate = state_id, hyper = precision.prior, constr = TRUE)"
 
-re_t07 <- "f(time_id, model = 'rw1', replicate = hr_id, hyper = precision.prior)"
-re_t08 <- "f(time_id, model = 'rw2', replicate = hr_id, hyper = precision.prior)"
-re_t09 <- "f(time_id, model = 'ar1', replicate = hr_id, hyper = precision.prior)"
+re_t07 <- "f(time_id, model = 'rw1', replicate = region_id, hyper = precision.prior, constr = TRUE)"
+re_t08 <- "f(time_id, model = 'rw2', replicate = region_id, hyper = precision.prior, constr = TRUE)"
+re_t09 <- "f(time_id, model = 'ar1', replicate = region_id, hyper = precision.prior, constr = TRUE)"
 
-re_t10 <- "f(time_id, model = 'rw1', replicate = biome_id, hyper = precision.prior)"
-re_t11 <- "f(time_id, model = 'rw2', replicate = biome_id, hyper = precision.prior)"
-re_t12 <- "f(time_id, model = 'ar1', replicate = biome_id, hyper = precision.prior)"
+re_t10 <- "f(time_id, model = 'rw1', replicate = biome_id, hyper = precision.prior, constr = TRUE)"
+re_t11 <- "f(time_id, model = 'rw2', replicate = biome_id, hyper = precision.prior, constr = TRUE)"
+re_t12 <- "f(time_id, model = 'ar1', replicate = biome_id, hyper = precision.prior, constr = TRUE)"
 
-re_t13 <- "f(time_id, model = 'rw1', replicate = koppen_id, hyper = precision.prior)"
-re_t14 <- "f(time_id, model = 'rw2', replicate = koppen_id, hyper = precision.prior)"
-re_t15 <- "f(time_id, model = 'ar1', replicate = koppen_id, hyper = precision.prior)"
+re_t13 <- "f(time_id, model = 'rw1', replicate = koppen_id, hyper = precision.prior, constr = TRUE)"
+re_t14 <- "f(time_id, model = 'rw2', replicate = koppen_id, hyper = precision.prior, constr = TRUE)"
+re_t15 <- "f(time_id, model = 'ar1', replicate = koppen_id, hyper = precision.prior, constr = TRUE)"
 
-re_t16 <- "f(time_id, model = 'rw1', hyper = precision.prior2)"
-re_t17 <- "f(time_id, model = 'rw2', hyper = precision.prior2)"
-re_t18 <- "f(time_id, model = 'ar1', hyper = precision.prior2)"
+re_t16 <- "f(time_id, model = 'rw1', hyper = precision.prior2, constr = TRUE, constr = TRUE)"
+re_t17 <- "f(time_id, model = 'rw2', hyper = precision.prior2, constr = TRUE, constr = TRUE)"
+re_t18 <- "f(time_id, model = 'ar1', hyper = precision.prior2, constr = TRUE, constr = TRUE)"
 
 # 5. Create lists for each type ----
 names(all_baseline) <- "re_baseline"
 
 # spatial
-all_s <- unlist(mget(c(paste0("re_s", sprintf("%02d", 1:2)))))
+all_s <- unlist(mget(c(paste0("re_s", sprintf("%02d", 1:4)))))
 names_s <- names(all_s)
 all_s <- paste0(" + ", all_s)
 names(all_s) <- names_s
@@ -137,16 +136,16 @@ names_w <- names(all_w)
 all_w <- paste0(" + ", all_w)
 names(all_w) <- names_w
 
-# temporal
-all_t <- unlist(mget(c(paste0("re_t", sprintf("%02d", 1:18)))))
-names_t <- names(all_t)
-all_t <- paste0(" + ", all_t)
-names(all_t) <- names_t
-
 # yearly
 all_y <- unlist(mget(c(paste0("re_y", sprintf("%02d", 1:18)))))
 names_y <- names(all_y)
 all_y <- paste0(" + ", all_y)
 names(all_y) <- names_y
+
+# temporal
+all_t <- unlist(mget(c(paste0("re_t", sprintf("%02d", 1:18)))))
+names_t <- names(all_t)
+all_t <- paste0(" + ", all_t)
+names(all_t) <- names_t
 
 rm(list = ls()[grepl("re", ls()) | grepl("names", ls())])
--- R/02_RE_univariate.R
+++ R/02_RE_univariate.R
@@ -20,27 +20,25 @@ library("dplyr")
 library("INLA")
 library("spdep")
 # library("GHRmodel") # Comment if in MN5
-# library("GHRpredict") # Comment if in MN5
 
 # Local packages in MN5 - comment if debugging
-library("GHRmodel", lib.loc = "/gpfs/scratch/bsc32/bsc498895/libraries/") 
-# library("GHRpredict", lib.loc = "/gpfs/scratch/bsc32/bsc498895/libraries/") 
+library("GHRmodel", lib.loc = "/gpfs/scratch/bsc32/bsc498895/libraries/")
 
 # Relative paths in MN5 - comment if debugging
 setwd("/gpfs/scratch/bsc32/bsc498895/sprint2025")
 
 # Read data, keep relevant columns for RE estimation
 data <- read.csv("data/processed/weekly_data.csv")
+
+# data <- data[data$year >= 2020,] # For debugging only
+
 data <- dplyr::select(data,
                       -starts_with("tas"), -starts_with("prlr"),
                       -starts_with("spi"), -starts_with("spei"),
                       -starts_with("nino"), -starts_with("oni"),
                       "nino_id") |> 
-  mutate(date = as.Date(date))
+  mutate(date = as.Date(date)) 
 glimpse(data)
-
-# For debugging only
-# data <- data[data$year >= 2018,]
 
 # 1. Formulas, priors, graph, utils ----
 
@@ -60,34 +58,34 @@ source("R/00_functions.R")
 
 # 2. Fit models to formulas ----
 
-# # Baseline model
-# print("Baseline")
-# sprint_mod(all_baseline, data, "", create_dir = T)
+# Baseline model
+print("Baseline")
+sprint_mod(all_baseline, data, "output/uni_re/", create_dir = T)
 
-# # Spatial RE
-# print("Spatial RE")
-# for(i in 1:length(all_s)){
-#   print(names(all_s)[i])
-#   sprint_mod(all_s[i], data, "", create_dir = T)
-# }
+# Spatial RE
+print("Spatial RE")
+for(i in 1:2){ # Replicated spatial RE are failing
+  print(all_s[i])
+  sprint_mod(all_s[i], data, "output/uni_re/", create_dir = T)
+}
 
 # Weekly RE
 print("Weekly RE")
-for(i in 8:length(all_w)){
-  print(names(all_w[i]))
-  sprint_mod(all_w[i], data, "", create_dir = T)
+for(i in 1:length(all_w)){
+  print(all_w[i])
+  sprint_mod(all_w[i], data, "output/uni_re/", create_dir = T)
 }
 
 # Yearly RE
 print("Yearly RE")
 for(i in 1:length(all_y)){
-  print(names(all_y[i]))
-  sprint_mod(all_y[i], data, "", create_dir = T)
+  print(all_y[i])
+  sprint_mod(all_y[i], data, "output/uni_re/", create_dir = T)
 }
 
 # Temporal RE
 print("Temporal RE")
 for(i in 1:length(all_t)){
-  print(names(all_t[i]))
-  sprint_mod(all_t[i], data, "", create_dir = T)
+  print(all_t[i])
+  sprint_mod(all_t[i], data, "output/uni_re/", create_dir = T)
 }
