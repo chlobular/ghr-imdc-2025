72ab90b061d14029d50253374234c9f91cd98947
Carles Milà <carles.milagarcia@bsc.es>
2025-07-01 14:06:53 +0200

    Univariate FE models

--- R/00_functions.R
+++ R/00_functions.R
@@ -11,95 +11,12 @@
 
 # ''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
 
-# create lagged variables
-# Borrowed from GHRmodel, to delete once I can install the dependency
-lag_cov <- function(data,
-                    name,
-                    time,
-                    lag,
-                    group = NULL,
-                    add   = TRUE) {
-  
-  ## 1) Checks 
-  if (missing(data) || missing(name) || missing(time) || missing(lag)) {
-    missing_args <- c()
-    if (missing(data))  missing_args <- c(missing_args, "data")
-    if (missing(name))   missing_args <- c(missing_args, "name")
-    if (missing(time))  missing_args <- c(missing_args, "time")
-    if (missing(lag))   missing_args <- c(missing_args, "lag")
-    stop("Missing required argument(s): ", paste(missing_args, collapse = ", "))
-  }
-  
-  if (!is.character(name))
-    stop("'name' must be a character vector.")
-  
-  if (!is.character(time) || length(time) != 1)
-    stop("'time' must be a single character string.")
-  
-  # `lag` must be a numeric vector ---------------------------
-  if (!is.numeric(lag) || any(lag < 0) || any(lag %% 1 != 0)) {
-    stop("'lag' must be a vector of positive integers.")
-  }
-  lag <- sort(unique(as.integer(lag)))  # ensure sorted, unique integers
-  
-  if (!all(c(name, time) %in% names(data))) {
-    missing_vars <- c(name, time)[!(c(name, time) %in% names(data))]
-    stop("The following 'name' or 'time' are not present in the dataset: ",
-         paste(missing_vars, collapse = ", "))
-  }
-  if (!is.null(group) && !all(group %in% names(data))) {
-    missing_groups <- group[!(group %in% names(data))]
-    stop("The following 'group' variables are not present in the dataset: ",
-         paste(missing_groups, collapse = ", "))
-  }
-  if (any(is.na(data[[time]])))
-    stop("The 'time' column contains missing values. Please clean it before lagging.")
-  
-  ## 2) ordering 
-  if (!is.null(group)) {
-    data <- data |>
-      dplyr::group_by(dplyr::across(tidyr::all_of(group))) |>
-      dplyr::arrange(dplyr::across(tidyr::all_of(c(group, time))))
-  } else {
-    data <- dplyr::arrange(data, .data[[time]])
-  }
-  
-  ## 3) create lagged columns 
-  lag_data <- data |>
-    dplyr::mutate(
-      dplyr::across(
-        .cols = tidyr::all_of(name),
-        .fns  = stats::setNames(
-          lapply(lag, function(k) function(x) dplyr::lag(x, n = k)),
-          paste0("l", lag)
-        ),
-        .names = "{.col}.{.fn}"
-      )
-    ) |>
-    dplyr::ungroup()
-  
-  ## 4) drop originals if add = FALSE 
-  if (!add) {
-    lag_cols <- paste0(rep(name, each = length(lag)),
-                       ".l",
-                       rep(lag, times = length(var)))
-    if (!all(lag_cols %in% names(lag_data)))
-      stop("Some lagged columns were not created as expected.")
-    
-    lag_data <- lag_data |>
-      dplyr::select(tidyr::all_of(lag_cols)) |>
-      as.matrix()
-  }
-  
-  return(lag_data)
-}
-
-
 #' Model selection for the dengue sprint
 #'
 #' @param formulas Named vector with INLA formulas
 #' @param data Data frame to fit the model to
 #' @param path Path where the output of the function will be saved to.
+#' @param filename String specifying the model name when writing to disk.
 #' @param create_dir If TRUE, create a directory in the path with name equal to 
 #' the formula name. Only possible if a single formula is supplied
 #' @param eval_train If TRUE, obtain GOF in the training data.
@@ -112,6 +29,7 @@ lag_cov <- function(data,
 sprint_mod <- function(formulas, 
                        data, 
                        path,
+                       filename = "train",
                        create_dir = FALSE,
                        eval_train = TRUE,
                        eval_test = FALSE){
@@ -131,14 +49,15 @@ sprint_mod <- function(formulas,
   # Fit model for the whole period, store it as rds. Store GOF metrics as csv.
   if(isTRUE(eval_train)){
     
+    mod_names <- ifelse(is.null(mod_names), "mod", mod_names)
     mod_all <- fit_models(formulas = GHRformulas,
                           data = data,
                           family = "nbinomial",
                           name = mod_names,
                           offset = "pop",
                           control_compute = list(config = FALSE, vcov = FALSE))
-    saveRDS(mod_all, paste0(path, "/train_mod.rds"))
-    write.csv(mod_all$mod_gof, paste0(path, "/train_gof.csv"), row.names = FALSE)
+    saveRDS(mod_all, paste0(path, "/", filename, "_mod.rds"))
+    write.csv(mod_all$mod_gof, paste0(path, "/", filename, "_gof.csv"), row.names = FALSE)
   }
   
   if(isTRUE(eval_test)){
@@ -154,4 +73,20 @@ sprint_mod <- function(formulas,
     
   }
   
+}
+
+
+
+write_FE_form <- function(FE, re_spatial = re_s, re_weekly = re_w, re_yearly = re_y){
+  form <- sapply(FE, function(x){
+    if(x!=""){
+      x2 <- paste(x, re_spatial, re_weekly, re_yearly, sep = " + ")
+      x2 <- paste0(" + ", x2)
+    }else{
+      x2 <- paste(re_spatial, re_weekly, re_yearly, sep = " + ")
+      x2 <- paste0(" + ", x2)
+    }
+    x2
+  })
+  form
 }
---
+++ R/04_FE_uni/run_tas.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --job-name=tas
+#SBATCH --output=tas%j.out
+#SBATCH --error=tas%j.err
+#SBATCH --ntasks=4
+#SBATCH --cpus-per-task=4
+#SBATCH --qos=gp_bsces
+#SBATCH --account=bsc32
+#SBATCH --mail-type=all
+#SBATCH --mail-user=carles.milagarcia@bsc.es
+
+# load modules
+module load R-bundle-Bioconductor/3.18-foss-2023b-R-4.3.3 
+module load R-bundle-CRAN/2023.12-foss-2023b 
+
+# Run script
+Rscript /gpfs/scratch/bsc32/bsc498895/sprint2025/R/04_FE_uni/04_FE_uni_tas.R
