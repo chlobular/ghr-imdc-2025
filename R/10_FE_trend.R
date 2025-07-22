5c8a0c729064a5114588c6c49e211a98a9f6bc7b
Carles Milà <carles.milagarcia@bsc.es>
2025-07-22 14:25:47 +0200

    Trend analysis report

--- R/10_FE_trend.R
+++ R/10_FE_trend.R
@@ -70,7 +70,7 @@ source("R/00_functions.R")
 
 
 # 2. RE ----
-re_s <- paste("f(hr_id, model = 'bym2', graph = g,",
+re_s <- paste("f(hr_id, model = 'bym2', replicate = nino_id, graph = g,",
               "scale.model = TRUE, hyper = precision.prior, constr = TRUE)")
 re_w <- paste("f(week_id, model = 'rw2', replicate = state_id, cyclic = TRUE,",
               "constr = TRUE, scale.model = TRUE, hyper = precision.prior)")
@@ -91,15 +91,15 @@ fe_base <- paste(cov_varying("v1", "koppen_id", ""),
 form_base <- paste("", fe_base, re_s, re_w, re_y, sep = " + ")
 
 # NS
-NS2 <- ns(data$time_id, df = 2)
-names(NS2) <- c("NS2_1", "NS2_2")
-saveRDS(NS2, "output/trend_fe/ns2.rds")
+ns2 <- ns(data$time_id, df = 2)
+colnames(ns2) <- c("ns2_1", "ns2_2")
+saveRDS(ns2, "output/trend_fe/ns2.rds")
 
-NS3 <- ns(data$time_id, df = 3)
-names(NS3) <- c("NS3_1", "NS3_2", "NS3_3")
-saveRDS(NS3, "output/trend_fe/ns3.rds")
+ns3 <- ns(data$time_id, df = 3)
+colnames(ns3) <- c("ns3_1", "ns3_2", "ns3_3")
+saveRDS(ns3, "output/trend_fe/ns3.rds")
 
-data <- cbind(data, NS2, NS3)
+data <- cbind(data, ns2, ns3)
 
 
 # Linear
@@ -115,43 +115,43 @@ f_linear_biome <- paste(form_base,
                         sep = " + ")
 
 # ns2
-f_ns2 <- paste(form_base, "NS2_1", "NS2_2", sep = " + ")
+f_ns2 <- paste(form_base, "ns2_1", "ns2_2", sep = " + ")
 f_ns2_koppen <- paste(form_base, 
-                      cov_varying("NS2_1", pattern = "", unit = "koppen_id8")[[1]],
-                      cov_varying("NS2_2", pattern = "", unit = "koppen_id9")[[1]],
+                      cov_varying("ns2_1", pattern = "", unit = "koppen_id8")[[1]],
+                      cov_varying("ns2_2", pattern = "", unit = "koppen_id9")[[1]],
                       sep = " + ")
 f_ns2_region <- paste(form_base, 
-                      cov_varying("NS2_1", pattern = "", unit = "region_id")[[1]],
-                      cov_varying("NS2_2", pattern = "", unit = "region_id2")[[1]],
+                      cov_varying("ns2_1", pattern = "", unit = "region_id")[[1]],
+                      cov_varying("ns2_2", pattern = "", unit = "region_id2")[[1]],
                       sep = " + ")
 f_ns2_biome <- paste(form_base, 
-                     cov_varying("NS2_1", pattern = "", unit = "biome_id")[[1]],
-                     cov_varying("NS2_2", pattern = "", unit = "biome_id2")[[1]],
+                     cov_varying("ns2_1", pattern = "", unit = "biome_id")[[1]],
+                     cov_varying("ns2_2", pattern = "", unit = "biome_id2")[[1]],
                      sep = " + ")
 
 # ns3
 f_ns3 <- paste(form_base, "ns3_1", "ns3_2", "ns3_3", sep = " + ")
 f_ns3_koppen <- paste(form_base, 
-                      cov_varying("NS3_1", pattern = "", unit = "koppen_id8")[[1]],
-                      cov_varying("NS3_2", pattern = "", unit = "koppen_id9")[[1]],
-                      cov_varying("NS3_3", pattern = "", unit = "koppen_id10")[[1]],
+                      cov_varying("ns3_1", pattern = "", unit = "koppen_id8")[[1]],
+                      cov_varying("ns3_2", pattern = "", unit = "koppen_id9")[[1]],
+                      cov_varying("ns3_3", pattern = "", unit = "koppen_id10")[[1]],
                       sep = " + ")
 f_ns3_region <- paste(form_base, 
-                      cov_varying("NS3_1", pattern = "", unit = "region_id")[[1]],
-                      cov_varying("NS3_2", pattern = "", unit = "region_id2")[[1]],
-                      cov_varying("NS3_3", pattern = "", unit = "region_id3")[[1]],
+                      cov_varying("ns3_1", pattern = "", unit = "region_id")[[1]],
+                      cov_varying("ns3_2", pattern = "", unit = "region_id2")[[1]],
+                      cov_varying("ns3_3", pattern = "", unit = "region_id3")[[1]],
                       sep = " + ")
 f_ns3_biome <- paste(form_base, 
-                     cov_varying("NS3_1", pattern = "", unit = "biome_id")[[1]],
-                     cov_varying("NS3_2", pattern = "", unit = "biome_id2")[[1]],
-                     cov_varying("NS3_3", pattern = "", unit = "biome_id3")[[1]],
+                     cov_varying("ns3_1", pattern = "", unit = "biome_id")[[1]],
+                     cov_varying("ns3_2", pattern = "", unit = "biome_id2")[[1]],
+                     cov_varying("ns3_3", pattern = "", unit = "biome_id3")[[1]],
                      sep = " + ")
 form_all <- c(form_base, f_linear, f_linear_koppen, f_linear_region, f_linear_biome,
               f_ns2, f_ns2_koppen, f_ns2_region, f_ns2_biome,
               f_ns3, f_ns3_koppen, f_ns3_region, f_ns3_biome)
 
 modtab <- data.frame(Models = c("Baseline", rep("Linear", 4), 
-                                rep("NS2", 4), rep("NS3", 4)),
+                                rep("ns2", 4), rep("ns3", 4)),
                      Replication = c("Baseline", 
                                      rep(c("None", "Koppen", "Region", "Biome"), 3)),
                      Formula = form_all)
@@ -160,5 +160,4 @@ write.csv(modtab, "output/trend_fe/modtab.csv", row.names = FALSE)
 
 # 4. Fit models ----
 
-## Without replicated spatial random effect
 sprint_mod(modtab$Formula, data, "output/trend_fe", "trend")
